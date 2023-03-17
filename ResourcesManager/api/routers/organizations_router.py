d54ee1d3145562f5763d57d830dd21dfe3806b76
Rafael Direito <rdireito@av.it.pt>
2023-03-17 17:20:01 +0000

    new endpoint

--- ResourcesManager/api/routers/organizations_router.py
+++ ResourcesManager/api/routers/organizations_router.py
@@ -3,7 +3,7 @@
 # @Email:  rdireito@av.it.pt
 # @Copyright: Insituto de Telecomunicações - Aveiro, Aveiro, Portugal
 # @Last Modified by:   Rafael Direito
-# @Last Modified time: 2023-03-17 09:48:04
+# @Last Modified time: 2023-03-17 17:19:34
 
 # generic imports
 from fastapi import (
@@ -50,6 +50,9 @@ router = APIRouter()
 def get_db():
     return next(main.get_db())
 
+@router.get("/hello")
+async def root():
+    return {"message": "Hello ESTGA Students!"}
 
 @router.post(
     "/organization/",
